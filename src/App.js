b59934f86df12feabf3047b283336ed0a2ca70f2
DinithiSamarakoon <dinithithamara@gmail.com>
2021-05-18 02:37:35 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import  './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+        height ="100vh"
+        projectID="80096117-2762-477a-8b7e-6464b73e4f98"
+        userName="javascriptmastery"
+        userSecret="java123"
+        />
+    );
+}
+
+export default App;
